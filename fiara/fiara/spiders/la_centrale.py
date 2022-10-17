f759aa549a5b8f23d3a1f515f0e3170de31e065d
Manitra RANDRIANASOLO <manitra.randrianasolo@raja.fr>
2022-10-17 17:59:17 +0200

    Follow link added

--- fiara/fiara/spiders/la_centrale.py
+++ fiara/fiara/spiders/la_centrale.py
@@ -20,12 +20,19 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self, response):
         soup = BeautifulSoup(response.text, features="lxml")
-        non_break_space = u'\xa0'
         cards = soup.find_all("div", {"class": "searchCard__rightContainer"})
-        for card in cards:
-            announce = {
-                "make_model": card.find("span", {"class": "searchCard__makeModel"}).get_text(),
-                "version": card.find("span", {"class": "searchCard__version"}).get_text(),
-                "price": unicodedata.normalize("NFKC", card.find("div", {"class": "searchCard__fieldPrice"}).find("span").get_text())
-            }
-            self.log(announce)
+
+        # for card in cards:
+        #     announce = {
+        #         "make_model": card.find("span", {"class": "searchCard__makeModel"}).get_text(),
+        #         "version": card.find("span", {"class": "searchCard__version"}).get_text(),
+        #         "price": unicodedata.normalize("NFKC", card.find("div", {"class": "searchCard__fieldPrice"}).find("span").get_text())
+        #     }
+        #     self.log(announce)
+
+        infos_cards = soup.find_all("section", {"class": "generalInformation"})
+
+        link_elements = soup.find_all("a", {"class": "searchCard__link"})
+        for link_elt in link_elements:
+            yield response.follow(link_elt['href'], callback=self.parse)
+
