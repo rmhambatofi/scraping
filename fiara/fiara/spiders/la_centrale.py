804aa8c41b41a3f9def560c6f5a78f50c09c6879
Manitra <manitra.randrianasolo@raja.fr>
2022-10-17 07:44:43 +0200

    Spider updated

--- fiara/fiara/spiders/la_centrale.py
+++ fiara/fiara/spiders/la_centrale.py
@@ -1,17 +1,31 @@
-import os
 
 import scrapy
+import unicodedata
+from bs4 import BeautifulSoup
 
 
 class QuotesSpider(scrapy.Spider):
-    name = "fiara"
+    name = "lacentrale"
 
-    def start_requests(self):
-        urls = [
-            'https://www.lacentrale.fr'
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = [
+        "https://www.lacentrale.fr/occasion-voiture-marque-volkswagen.html",
+    ]
+
+    # def start_requests(self):
+    #     urls = [
+    #         'https://www.lacentrale.fr/occasion-voiture.html'
+    #     ]
+    #     for url in urls:
+    #         yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        self.log('========')
+        soup = BeautifulSoup(response.text, features="lxml")
+        non_break_space = u'\xa0'
+        cards = soup.find_all("div", {"class": "searchCard__rightContainer"})
+        for card in cards:
+            announce = {
+                "make_model": card.find("span", {"class": "searchCard__makeModel"}).get_text(),
+                "version": card.find("span", {"class": "searchCard__version"}).get_text(),
+                "price": unicodedata.normalize("NFKC", card.find("div", {"class": "searchCard__fieldPrice"}).find("span").get_text())
+            }
+            self.log(announce)
